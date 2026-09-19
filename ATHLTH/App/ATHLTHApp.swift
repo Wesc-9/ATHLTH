5b1e2bc65d0d02b3fab35d30055d18979cd1ab9b
Wesc-9 <stian.r.johansen@outlook.com>
2026-09-19 23:54:33 +0200

    Wire product foundation into ATHLTH app

--- ATHLTH/App/ATHLTHApp.swift
+++ ATHLTH/App/ATHLTHApp.swift
@@ -4,23 +4,26 @@ import SwiftUI
 struct ATHLTHApp: App {
     @StateObject private var health = HealthKitManager()
     @StateObject private var trainingPlan = TrainingPlanStore()
+    @StateObject private var appSession = AppSessionStore()
 
     var body: some Scene {
         WindowGroup {
             AppRootView()
                 .environmentObject(health)
                 .environmentObject(trainingPlan)
+                .environmentObject(appSession)
         }
     }
 }
 
 struct AppRootView: View {
     @EnvironmentObject private var health: HealthKitManager
+    @EnvironmentObject private var appSession: AppSessionStore
 
     var body: some View {
         Group {
-            if health.hasRequestedAuthorization {
-                RootTabView()
+            if health.hasRequestedAuthorization || appSession.previewModeEnabled {
+                ProductRootTabView()
             } else {
                 HealthAccessView()
             }
