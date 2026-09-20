04c4118674fb9f17c792a851fa46efb85ed58289
Wesc-9 <stian.r.johansen@outlook.com>
2026-09-20 13:18:18 +0200

    Handle iPhone-started workouts on Apple Watch

--- ATHLTHWatchExtension/ATHLTHWatchApp.swift
+++ ATHLTHWatchExtension/ATHLTHWatchApp.swift
@@ -1,14 +1,31 @@
+import HealthKit
 import SwiftUI
+import WatchKit
 
 @main
 struct ATHLTHWatchApp: App {
+    @WKApplicationDelegateAdaptor(WatchAppDelegate.self)
+    private var appDelegate
+
     @StateObject private var routeStore = WatchRouteStore()
+    @StateObject private var workoutManager = WatchWorkoutManager.shared
 
     var body: some Scene {
         WindowGroup {
             WatchHomeView()
                 .environmentObject(routeStore)
+                .environmentObject(workoutManager)
                 .preferredColorScheme(.light)
+        }
+    }
+}
+
+final class WatchAppDelegate: NSObject, WKApplicationDelegate {
+    func handle(_ workoutConfiguration: HKWorkoutConfiguration) {
+        Task {
+            await WatchWorkoutManager.shared.start(
+                configuration: workoutConfiguration
+            )
         }
     }
 }
